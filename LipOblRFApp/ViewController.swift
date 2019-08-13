3b396fce215314eb8248faeb7618ca84e7a0cf53
ProMindSl <hot_peachy@hotmail.com>
2019-08-13 17:47:21 +0300

    Initial Commit

---
+++ LipOblRFApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LipOblRFApp
+//
+//  Created by Viatcheslav Avdeev on 13/08/2019.
+//  Copyright © 2019 Viatcheslav Avdeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
