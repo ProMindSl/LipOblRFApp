c7f26dc725bdcf7889852f06477a74481c80fa52
ProMindSl <hot_peachy@hotmail.com>
2019-08-22 16:20:44 +0300

    change navigate root init

---
+++ LipOblRFApp/Controllers/RootViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RootViewController.swift
+//  LipOblRFApp
+//
+//  Created by Viatcheslav Avdeev on 22/08/2019.
+//  Copyright © 2019 Viatcheslav Avdeev. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController
+{
+    
+}
