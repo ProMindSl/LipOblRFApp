0e2aa9c68b0661b864104f466ea3a8412180819f
ProMindSl <hot_peachy@hotmail.com>
2019-08-22 17:23:05 +0300

    add nav logic

--- LipOblRFApp/Controllers/RootViewController.swift
+++ LipOblRFApp/Controllers/RootViewController.swift
@@ -10,5 +10,26 @@ import UIKit
 
 class RootViewController: UIViewController
 {
+    private var current: UIViewController
     
+    init()
+    {
+        self.current = NewsViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self) 
+    }
 }
