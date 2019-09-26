9561db602a8195f4529853175ae87ecde7bf45b4
ProMindSl <hot_peachy@hotmail.com>
2019-09-26 09:51:40 +0300

    configure ui elements in SignInController

--- LipOblRFApp/Controllers/SignInViewController.swift
+++ LipOblRFApp/Controllers/SignInViewController.swift
@@ -15,13 +15,45 @@ class SignInViewController: UIViewController
     @IBOutlet weak var btnSignUp: UIButton!
     
     
-    
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        addListeners()
+    }
+    
+    private func addListeners()
+    {
+        // configure ui view
+        btnSignIn.layer.cornerRadius = 6
+        btnSignUp.layer.cornerRadius = 6
+        btnSignUp.layer.borderColor = UIColor.white.cgColor
+        btnSignUp.layer.borderWidth = CGFloat(exactly: 1.0)! 
+        
+        [btnSignIn, btnSignUp].forEach(
+        {
+            $0?.addTarget(self, action: #selector(didSelect(_:)), for: .touchUpInside)
+        })
+    }
+    
+    /*
+    *   btn click methods
+    **/
+    @objc func didSelect(_ sender: UIButton)
+    {
+        let rootVC = AppDelegate.shared.rootViewController
+        
+        switch sender
+        {
+        case btnSignIn :
+            rootVC.showLoginScreen()
+        case btnSignUp:
+            break
+        default:
+            break
+        }
     }
     
 
 }
+
+
